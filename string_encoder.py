780f01d23b702b4cf1388214fa85dfc016bfa89b
Mala <m.nimalasuriya@gmail.com>
2016-10-20 17:07:46 -0700

    begin outlining problem

---
+++ string_encoder.py
@@ -0,0 +1,11 @@
+# writing out pseudo code to get started
+
+class HL7String:
+# define/set attributes somewhere here
+
+# include a function parse that takes in the PID and PV1 params and returns a string with that data
+    def parse():
+        # do something with our predefined or set attributes
+    return # an HL7-encoded string
+
+# include an example patient record for testing in the console
